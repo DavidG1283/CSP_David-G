d2686245d8d12dbba90e831ec81a2a54b163a0d4
DavidG1283 <david.garciabermudez@ucas-edu.net>
2025-01-29 11:42:56 -0700

    python notes

---
+++ Variables/notes.py
@@ -0,0 +1,16 @@
+# David Garcia, Variables Notes
+
+name = "Garcia"
+
+subject = input("What's your favorite subject: ")
+
+small_number = 4
+
+print("It worked!")
+print(subject, "is the teacher of", name)
+
+print(name, "is my favorite teacher")
+
+name = "Papacito david"
+
+print(name, "favorite subject is", subject)
