ffa05bfb827a073979a0181432d87da432bc8e02
DavidG1283 <david.garciabermudez@ucas-edu.net>
2025-02-04 11:17:09 -0700

    finishing notes

---
+++ notes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+char name[] = "Garcia";
+int age = 15;
+float pi = 3.141596;
+
+int main(void){
+    printf("%s\n", name);
+    printf("%d\n", age);
+    printf("%f\n", pi);
+    printf("Hi I am %s I am ")
+    return 0;
+}
---
+++ practice.py
@@ -0,0 +1,11 @@
+# David Garcia, Variable notes practice\
+
+name = "David"
+
+number_between_1and10 = "6"
+print(number_between_1and10)
+number_between_100and1000 = 150
+
+breakfast = input("What did you have for breakfast: ")
+
+print(name, "Breakfast was", breakfast)
