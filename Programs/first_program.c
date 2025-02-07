3e2a297c43fd14c7d33f8dca0fcb14e3b34834de
DavidG1283 <david.garciabermudez@ucas-edu.net>
2025-02-07 11:20:37 -0700

    Fixed first program

---
+++ Programs/first_program.c
@@ -0,0 +1,13 @@
+// David Garcia First program
+
+#include <stdio.h>
+
+int main() {
+
+    printf("Hello World and welcome to my first program. \n");
+    char name[100];
+    printf("Enter your name: ");
+    scanf("%s", name);
+    printf("Hello %s! Welcome to my program.\n", name);
+    return 0;
+}
---
+++ Programs/first_program.py
@@ -0,0 +1,7 @@
+# David garcia First program
+print("Hello World")
+name = input("What is your name?: ")
+school = input("What school do you go to?: ")
+
+print("Hello", name, "I'm glad you are a", school, "student")
+print("Welcome to my program")
