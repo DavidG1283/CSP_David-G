20b7988283e2308cc7dc61f0e51f8c6f4852d08a
DavidG1283 <david.garciabermudez@ucas-edu.net>
2025-02-10 11:49:30 -0700

    notes strings only python

---
+++ strings/notes.py
@@ -0,0 +1,17 @@
+#David Garcia Strings notes python
+
+#string is a data type that holds information surrounded by quotation marks
+
+note = "david's class"
+
+name = input("What is your first name?\n").strip().capitalize()
+#Capitalize capitalizes only the first letter of the answer
+#upper all letter of the answer
+#lower lowercase all the letters of the answer
+print(f"Hello {name} welcome to my program")
+
+sentence = "The quick brown fox jumps over the lazy dog"
+
+num1 = 2 
+num = 4
+print(num1 + num)
