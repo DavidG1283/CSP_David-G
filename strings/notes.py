2c6bb6405b014e574a62d0bd0d34fa97621d779a
DavidG1283 <david.garciabermudez@ucas-edu.net>
2025-02-11 11:15:48 -0700

    End strings notes python

--- strings/notes.py
+++ strings/notes.py
@@ -2,16 +2,23 @@
 
 #string is a data type that holds information surrounded by quotation marks
 
-note = "david's class"
+#note = "david's class"
 
-name = input("What is your first name?\n").strip().capitalize()
+#name = input("What is your first name?\n").strip().capitalize()
 #Capitalize capitalizes only the first letter of the answer
 #upper all letter of the answer
 #lower lowercase all the letters of the answer
-print(f"Hello {name} welcome to my program")
+#print(f"Hello {name} welcome to my program")
 
-sentence = "The quick brown fox jumps over the lazy dog"
+sentence = "The quick brown fox jumps over the lazy dog."
 
-num1 = 2 
-num = 4
-print(num1 + num)
+#num1 = 2 
+#num = 4
+#print(num1 + num)
+#print('characters in that sentence')
+#print('There are') 
+#print(len(sentence))
+start = sentence.find("brown")
+print(sentence.find("brown"))
+length = len("brown fox")
+print(sentence[10:19])
