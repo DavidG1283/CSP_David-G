78adbf317127ce7a36336d8c5325d9b450ad6238
DavidG1283 <david.garciabermudez@ucas-edu.net>
2025-02-11 11:49:30 -0700

    notes py and c

---
+++ strings/notes.c
@@ -0,0 +1,32 @@
+// David Garcia, Strings notes C 
+#include <stdio.h>
+#include <string.h>
+char subject[50];
+char name[] = "Victoria";
+char sentence[] = "The quick brown fox jumps over the lazy dog.";
+int main(void){
+//printf("What class are you in?\n");
+//scanf("%s", subject);
+//fgets(subject, sizeof(subject), stdin);
+// printf("You are in %s, that is a cool class", subject);
+//name[0] = 'T';
+//name[1] = 'o';
+//name[2] = 'r';
+//name[3] = 'i';
+//name[4] = ' ';
+//name[5] = ' ';
+//name[6] = ' ';
+//name[7] = ' ';
+//name[8] = ' ';
+//printf("%lu"sizeof(sentence));
+//printf("%d"strlen(sentence));
+char one[] = " Hello";
+char two[] = " World!";
+char three[] = "Welcome to my program";
+printf("%s\n", one);
+strcat(one, two);
+printf("%s\n", one);
+strcat(three, one);
+printf("%s\n", three);
+    return 0;
+}
