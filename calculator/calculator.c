1c7cfbcd92c7de96c8a088cb12d441c0f6dd3317
DavidG1283 <david.garciabermudez@ucas-edu.net>
2025-02-19 11:49:03 -0700

    calc c alm fin

--- calculator/calculator.c
+++ calculator/calculator.c
@@ -3,28 +3,60 @@
 #include <math.h>
 
 float income;
-float rent;
-float transportation;
-float groceries;
-float utilities;
-int main(void){
-printf("Hello user, welcome to my calculator, here you will calculate how much percentage you should put into saving and a lot more, HAVE FUN!!!!!!!!!");
-printf("what is your income?:\n");
-scanf("%f", income);
 
-float save = income *.1;
+float rent;
+
+float transportation;
+
+float groceries;
+
+float utilities;
+
+float save;
+
+float rentpercent;
+
+float transpercent;
+
+float grocpercent;
+
+float utilpercent;
+
+int main(void){
+
+printf("Hello user, welcome to my calculator, here you will calculate how much percentage you should put into saving and a lot more, HAVE FUN!!!!!!!!!\n");
+
+printf("What is your income?:\n");
+
+scanf("%f", &income);
 
 printf("What is your rent?:\n");
-scanf("%f", rent);
+
+scanf("%f", &rent);
 
 printf("What is your Trans?:\n");
-scanf("%f", transportation);
+
+scanf("%f", &transportation);
 
 printf("What is your Gro?:\n");
-scanf("%f", groceries);
+
+scanf("%f", &groceries);
 
 printf("What is your Uti?:\n");
-scanf("%f", utilities);
 
+scanf("%f", &utilities);
+
+save = income*.1;
+
+printf("You should save 10 percent of your income which is\n");
+printf("%.2f", save);
+
+
+rentpercent = rent / income * 100;
+transpercent = transportation / income * 100;
+grocpercent = groceries / income * 100;
+utilpercent = utilities / income * 100;
+
+printf("Rent is %s of your rent", rentpercent);
     return 0;
 }
