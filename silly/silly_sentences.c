7134a3eab3e8d8b7d6c918ec59b6b3865d46e89c
davicito tu papacito <david.garciabermudez@ucas-edu.net>
2025-02-21 00:34:25 -0600

    Update silly_sentences.c

--- silly/silly_sentences.c
+++ silly/silly_sentences.c
@@ -1,12 +1,23 @@
 // David Garcia, Silly C 
 #include <stdio.h>
+#include <string.h>
 
-int main(void){
-    int name = input("What is your name?:");
+int main() {
+    char silly1[100], silly2[100], silly3[100];
+    char story[500];
+    printf("Enter a silly word (e.g., dog, car, pizza): ");
+    fgets(silly1, sizeof(silly1), stdin);
+    silly1[strcspn(silly1, "\n")] = 0;
 
-    printf("Hello" "%s", "welcome to my program, in this porgram you will create silly sentences with your own set of silly words, I hope you have fun!!!\n");
-    scanf("Say the first silly word");
-    
-    
+    printf("Enter a silly word (e.g., funny, silly, purple): ");
+    fgets(silly2, sizeof(silly2), stdin);
+    silly2[strcspn(silly2, "\n")] = 0;
+
+    printf("Enter a silly word (e.g., run, jump, eat): ");
+    fgets(silly3, sizeof(silly3), stdin);
+    silly3[strcspn(silly3, "\n")] = 0;
+    snprintf(story, sizeof(story), "Once upon a time, a %s %s decided to %s all the way to the moon.", silly2, silly1, silly3);
+    printf("%s\n", story);
+
     return 0;
 }
