3c0d4c8dd48c397dde03fc57d07ca73d2cf1c3a8
davicito tu papacito <david.garciabermudez@ucas-edu.net>
2025-02-21 00:14:56 -0600

    Update name_decorator.py

--- name_decorator/name_decorator.py
+++ name_decorator/name_decorator.py
@@ -1,12 +1,16 @@
 # David Garcia Name decorator Py
 name = input("What is your name?:\n")
 
-print(f"<<{name}>>")
-print(f"##{name}##")
-print(f"\\{name}//")
-print(f"$${name}$$")
-print(f"%%{name}%%")
-print(f"@@{name}@@")
-print(f"^^{name}^^")
+decoratedname1 = f"<<{name}>>"
+decoratedname2 = f"##{name}##"
+decoratedname3 = f"\\{name}//"
+decoratedname4 = f"$$ {name} $$"
+decoratedname5 = f"%%{name}%%"
+decoratedname6 = f"@@{name}@@"
+decoratedname7 = f"^^{name}^^"
 
+# Concatenate the decorated names in a single output
+alldecoratednames = decoratedname1 + " " + decoratedname2 + " " + decoratedname3 + " " + decoratedname4 + " " + decoratedname5 + " " + decoratedname6 + " " + decoratedname7
+
+print(alldecoratednames)
 print("Thanks for using this program to make your name a little bit silly by decorating it, I hope yu had fun!!!")
