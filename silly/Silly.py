66fe75501e35f82f619397526f03514811df70cc
davicito tu papacito <david.garciabermudez@ucas-edu.net>
2025-02-21 00:22:32 -0600

    Create Silly.py

---
+++ silly/Silly.py
@@ -0,0 +1,9 @@
+# Sily sentences Python David Garcia
+
+silly1 = input("Enter a silly word (e.g., dog, car, pizza): ")
+silly2 = input("Enter a silly word (e.g., funny, silly, purple): ")
+silly3 = input("Enter a silly word (e.g., run, jump, eat): ")
+
+story = "Once upon a time, a " + adjective + " " + noun + " decided to " + verb + " all the way to the moon."
+
+print(story)
