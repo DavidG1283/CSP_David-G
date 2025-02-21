01399bf8255e7472c54f7dacc5cfe98066f26ad5
DavidG1283 <david.garciabermudez@ucas-edu.net>
2025-02-21 11:36:45 -0700

    notes C func

---
+++ functions/notes.c
@@ -0,0 +1,19 @@
+// David Garcia Notes C functions
+
+
+void due(char assignment[40], char day[30]) {
+    printf("The %s assignment is due %s",assignment,day);
+
+
+   
+}
+
+
+int main(void) {
+    due("Functions notes","today\n");
+    due("Hello world update","tomorrow\n");
+    due("Financial calculator update","friday\n");
+
+
+    return 0;
+}
