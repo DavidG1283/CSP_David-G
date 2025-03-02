cfb7eab4f1a5ab62a608237fe9acdb0c2af71c5d
davicito tu papacito <david.garciabermudez@ucas-edu.net>
2025-03-01 21:29:44 -0600

    Create update.py

---
+++ Programs/update.py
@@ -0,0 +1,19 @@
+# David garcia First program
+print("Hello World")
+
+name = input("What is your name?: ")
+school = input("What school do you go to?: ")
+
+print(f"Hello {name}, I'm glad you are a {school} student.")
+print("Welcome to my program!")
+
+name1 = input("Please enter name 1: ")
+name2 = input("Please enter name 2: ")
+name3 = input("Please enter name 3: ")
+name4 = input("Please enter name 4: ")
+name5 = input("Please enter name 5: ")
+
+def add(name1, name2, name3, name4, name5):
+    print("Hello", name1, name2, name3, name4, name5, "welcome!!")
+
+add(name1, name2, name3, name4, name5)
