a38d8ad3829583265093716f9f6244df42259095
DavidG1283 <david.garciabermudez@ucas-edu.net>
2025-03-07 11:33:52 -0700

    fizzpy

---
+++ loops/fizz.py
@@ -0,0 +1,11 @@
+# David Garcia FizzBuzz Py
+x = 1
+while x < 50:
+    print(x)
+    x+=1
+    if x % 3 == 0 and x % 5 == 0:  
+        print("FizzBuzz")  
+    elif x % 3 == 0:  
+        print("Fizz")  
+    elif x % 5 == 0:  
+        print("Buzz") 
