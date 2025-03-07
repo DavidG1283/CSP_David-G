d76485b607b19a9440041f9c06d33618156be73f
DavidG1283 <david.garciabermudez@ucas-edu.net>
2025-03-07 11:49:07 -0700

    fizzcstar

---
+++ loops/fizz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+    int x = 1;
+while(x < 50);
+    printf(x);
+    x+=1;{
+    }if (x % 3 == 0 && x % 5 == 0){
+        printf("FizzBuzz");
+    }else if (x % 3 == 0){
+        printf("Fizz"); 
+    }else if (x % 5 == 0)
+        printf("Buzz");
+x+=1;
+
+
+     return 0;   
+    }
+
