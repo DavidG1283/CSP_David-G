2c9c999ff5532185c884eab8f6b275c92b9ac567
DavidG1283 <david.garciabermudez@ucas-edu.net>
2025-03-20 11:18:59 -0600

    fizzzzz

--- loops/fizz.c
+++ loops/fizz.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
-
+int x = 1;
 int main(void){
-    int x = 1;
-while(x < 50);
-    printf(x);
-    x+=1;{
-    }if (x % 3 == 0 && x % 5 == 0){
-        printf("FizzBuzz");
+
+while(x < 51){
+    if (x % 3 == 0 && x % 5 == 0){
+        printf("FizzBuzz\n");
     }else if (x % 3 == 0){
-        printf("Fizz"); 
-    }else if (x % 5 == 0)
-        printf("Buzz");
+        printf("Fizz\n"); 
+    }else if (x % 5 == 0){
+        printf("Buzz\n");
+    }else{
+        printf("%d\n",x);
+    }
 x+=1;
 
+
+
+
+
+};
 
      return 0;   
     }
