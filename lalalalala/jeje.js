a4d9bd782a93768331f3ef5ae4be117abfe60708
DavidG1283 <david.garciabermudez@ucas-edu.net>
2025-04-28 11:29:55 -0600

    jeje

---
+++ lalalalala/jeje.js
@@ -0,0 +1,13 @@
+// JS\
+
+function hello(){
+    document.getElementById("title").innerHTML = "Hello World"
+}
+
+function change() {
+    document.getElementById("image").src = "https://purina.cl/sites/default/files/2022-11/purina-brand-cuanto-vive-un-gato-nota_03.jpg"
+}
+
+function out() {
+        document.getElementById("lele").src ="https://static.nationalgeographic.es/files/styles/image_3200/public/nationalgeographic_1468962.webp?w=1600&h=900"
+}
