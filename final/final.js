57a6bfcce2745b61fdb987a388ca466ea06ef6e5
DavidG1283 <david.garciabermudez@ucas-edu.net>
2025-04-29 13:31:22 -0600

    midfinal

---
+++ final/final.js
@@ -0,0 +1,4 @@
+function zion(){
+    document.getElementById("zion24").src = "https://www.stgeorgeutahvacationrentals.com/media/654141b29e1a1eee2ee65b56/xlarge.webp"
+    document.getElementById("zion3").innerHTML = "Zion is renowned for its towering sandstone cliffs, lush green valleys, and the expansive canyon, providing breathtaking vistas"
+}
--- lalalalala/jeje.js
+++ lalalalala/jeje.js
@@ -1,6 +1,7 @@
 // JS\
 
 function lirili(){
+    document.getElementById("image").src = "https://www.stgeorgeutahvacationrentals.com/media/654141b29e1a1eee2ee65b56/xlarge.webp"
     document.getElementById("lala").innerHTML = "Cats are popular pets valued for their companionship, playfulness, and ability to kill vermin. They are domesticated members of the Felidae family, with the domestic cat (Felis catus) being the only domesticated species within the family. "
 }
 
