ec6288c43ebf3f66b86a924166a9f206317c93c8
DavidG1283 <david.garciabermudez@ucas-edu.net>
2025-05-01 13:26:08 -0600

    finfinfin

--- final/final.js
+++ final/final.js
@@ -1,8 +1,24 @@
-function zion(){
-    document.getElementById("zion4").src="https://www.usatoday.com/gcdn/authoring/authoring-images/2024/07/03/USAT/74297807007-the-narrows-03128.jpg?crop=8632,4859,x0,y899&width=3200&height=1802&format=pjpg&auto=webp"
-    document.getElementById("zion3").innerHTML = "Follow the paths where people have walked for thousands of years. Gaze up at massive sandstone cliffs of cream, pink, and red that soar into a brilliant blue sky. Experience the tall, stretching walls of a narrow slot canyon. Zion’s unique array of plants and animals will enchant you as you absorb the rich history of the past and enjoy the excitement of present-day adventures."
-}
 
-function change() {
+function change(){
     document.getElementById("zion5").src ="https://lh3.googleusercontent.com/gps-cs-s/AC9h4nqU52aWct96e_npjK0W52kz6kALR8gBFgmAR_m3sbQkFlbjtftGNiyzVH2TdoHBoy4IcvN8VuX7fu312tH5XXea3d90xY8dzMOLsdqA22So2gk8tpVTF4BGVBk4_RluOGjo5yBY=s680-w680-h510-rw"
 }
+
+function zion() {
+    var x = document.getElementById("div");
+//    document.getElementById("zion4").src="https://www.usatoday.com/gcdn/authoring/authoring-images/2024/07/03/USAT/74297807007-the-narrows-03128.jpg?crop=8632,4859,x0,y899&width=3200&height=1802&format=pjpg&auto=webp"
+//    document.getElementById("zion3").innerHTML = "Follow the paths where people have walked for thousands of years. Gaze up at massive sandstone cliffs of cream, pink, and red that soar into a brilliant blue sky. Experience the tall, stretching walls of a narrow slot canyon. Zion’s unique array of plants and animals will enchant you as you absorb the rich history of the past and enjoy the excitement of present-day adventures."
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }  
+}
+
+
+
+
+
+
+
+
+
