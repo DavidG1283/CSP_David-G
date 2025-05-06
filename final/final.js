14cb404b7af54cc86dff3f03000f2c0a1b8889d2
davicito tu papacito <david.garciabermudez@ucas-edu.net>
2025-05-05 22:52:24 -0500

    Update final.js

--- final/final.js
+++ final/final.js
@@ -1,5 +1,3 @@
-document.getElementById("hover-zion").addEventListener("mouseover", function() { this.src = "https://lh3.googleusercontent.com/gps-cs-s/AC9h4nqU52aWct96e_npjK0W52kz6kALR8gBFgmAR_m3sbQkFlbjtftGNiyzVH2TdoHBoy4IcvN8VuX7fu312tH5XXea3d90xY8dzMOLsdqA22So2gk8tpVTF4BGVBk4_RluOGjo5yBY=s680-w680-h510-rw"; });
-document.getElementById("hover-zion").addEventListener("mouseout", function() { this.src = "https://lh3.googleusercontent.com/gps-cs-s/AC9h4npqkMwnsnSsP8RC9ic59lrIF8ZFW7IXPsorortkqKLvLrGIrxxwMG2JLltb7YLCoOx_mIvRA_lztTXbZJyxYkRPNr3MvXUt9paKfaYAZwDJPC1skld-pJKwXBHINttJ7YpnWtTp=s680-w680-h510-rw"; }); 
 
 function zion() {
     var x = document.getElementById("additional-text");
